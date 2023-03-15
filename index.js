a5e283e7b8d8b821a95411dbce26cb43edb79a57
Mary Wanjiku <er.muthii@gmail.com>
2023-03-15 13:13:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser='Eric Macharia'
+const welcomeMessage='Welcome to Flatbook!'
+
+welcomeMessage=currentUser
+
+excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser}!`
+
+const shortGreeting=`Welcome, ${currentUser}`
